9c876e6453e5b81891b7ac4f72a1031527e43bfe
NickHarshman <nickh25@vt.edu>
2016-07-18 12:17:57 -0400

    Added loot spawner
    spawns loot

--- Boderlance/Assets/Items.cs
+++ Boderlance/Assets/Items.cs
@@ -1,19 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Item : MonoBehaviour
-{
+public class Items : MonoBehaviour {
+
     private int id;
     private Rigidbody2D rb;
-    private Vector2 splosion = new Vector2(Random.Range(0.1f, 0.5f), Random.Range(0.1f, 0.5f));
+    private Vector2 splosion = new Vector2(Random.Range(-1f, 1f), Random.Range(0f, 3f));
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = splosion;
     }
-            
-    
-
-    
 }
-	
---
+++ Boderlance/Assets/Spawn.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+    private Transform point;
+    private float rng;
+    public GameObject item;
+    public GameObject money;
+    public GameObject common;
+    public GameObject uncommon;
+    public GameObject rare;
+    public GameObject veryrare;
+    public GameObject legendary;
+    // Use this for initialization
+    void Start () {
+        point = GetComponent<Transform>();
+        InvokeRepeating("Loot", .2f, .2f);
+	}
+	
+	// Update is called once per frame
+	void Loot()
+    {
+        rng = Random.Range(0f, 1f);
+        if(rng<.3f)
+        {
+            Instantiate(item, point.position, point.rotation);
+        }
+        else
+        {
+            if(rng<.6f)
+            {
+                Instantiate(money, point.position, point.rotation);
+            }
+            else
+            {
+                if(rng<.8f)
+                {
+                    Instantiate(common, point.position, point.rotation);
+                }
+                else
+                {
+                    if(rng<.9f)
+                    {
+                        Instantiate(uncommon, point.position, point.rotation);
+                    }
+                    else
+                        if(rng<.95f)
+                    {
+                        Instantiate(rare, point.position, point.rotation);
+                    }
+                    else
+                    {
+                        if(rng<.98f)
+                        {
+                            Instantiate(veryrare, point.position, point.rotation);
+                        }
+                        else
+                        {
+                            Instantiate(legendary, point.position, point.rotation);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
